9416fbec218b631ed1b504725cb364889d39b7eb
msagolj <msagolj@adobe.com>
2024-10-24 16:20:47 +0200

    remove decorateSectionFromMetadata

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,7 +5,6 @@ import {
   decorateButtons,
   decorateIcons,
   decorateSections,
-  decorateSectionFromMetadata,
   decorateTemplateAndTheme,
   loadBlocks,
   loadCSS,
@@ -206,7 +205,6 @@ export function decorateMain(main) {
   decorateButtons(main);
   decorateIcons(main);
   decorateSections(main);
-  decorateSectionFromMetadata(main); // custom
   scheduleSections(main);
   scheduleBlocks(main);
   decorateBlocks(main);
